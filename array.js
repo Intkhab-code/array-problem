0438f0d532c4f59c4da528cd2654ed69226e9e90
intkhab alam <intkhabalam@gmail.com>
2026-01-27 21:31:12 +0530

    add in github

---
+++ array.js
@@ -0,0 +1,17 @@
+// 1. TERNARY OPERATOR
+// function myfunc(num){
+//     return Number(num%2===0) ? "even": "odd"
+
+// }
+// const result = myfunc("000014")
+// console.log(result);
+
+// 2. CHANGE THE POSITION OF INDEX ELEMENT
+// function changeArrayEleIndex(arr, a, c){
+//  [arr[a], arr[c]] = [arr[c], arr[a]];
+//  return arr;
+// }
+// const arr= [3,6,7,1,3,2,4,5,9]
+// const result = changeArrayEleIndex(arr, 3, 4);
+// console.log(result);
+
